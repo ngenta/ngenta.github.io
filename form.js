8f98044c50a2c9131c1d7ae303c60e97ab6fda0b
ngenta <38299322+ngenta@users.noreply.github.com>
2018-07-26 17:31:03 -0300

    v2

--- form.js
+++ form.js
@@ -1,26 +1,26 @@
 $(document).ready(function(){
 $(function($){
-		$("#form").submit(function(event){
-			event.preventDefault();
-				$.ajax({
-			    url: "https://formspree.io/nachofuentes7164@gmail.com", 
-			    method: "POST",
-			    data: {
-			    	nombre: $("#nombre").val(),
-			    	email: $("#email").val(),
-			    	asunto: $("#asunto").val(),
-			    	mensaje: $("#textarea").val(),
-				},
-			    dataType: "json"
-				}).done(function(){
-					$("#nombre").val("");
-			    	$("#email").val("");
-			    	$("#asunto").val("");
-			    	$("#textarea").val("");
-					$(".formresponse").addClass("text-success").text("Mensaje enviado con éxito")					
-				}).fail(function(){
-					$(".formresponse").addClass("text-danger").text("Hubo un error en el envío de tu mensaje")
-				});
-		});
+    $("#form").submit(function(event){
+      event.preventDefault();
+        $.ajax({
+          url: "https://formspree.io/nachofuentes7164@gmail.com", 
+          method: "POST",
+          data: {
+            nombre: $("#nombre").val(),
+            email: $("#email").val(),
+            asunto: $("#asunto").val(),
+            mensaje: $("#textarea").val(),
+        },
+          dataType: "json"
+        }).done(function(){
+          $("#nombre").val("");
+            $("#email").val("");
+            $("#asunto").val("");
+            $("#textarea").val("");
+          $(".formresponse").addClass("text-success").text("Mensaje enviado con éxito")         
+        }).fail(function(){
+          $(".formresponse").addClass("text-danger").text("Hubo un error en el envío de tu mensaje")
+        });
+    });
 });
     });
