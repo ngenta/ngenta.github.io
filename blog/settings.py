f378c832ff4f6a64ba9f276c7f9ae70a9514ab75
ngenta <38299322+ngenta@users.noreply.github.com>
2021-10-09 01:16:10 -0300

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -44,7 +44,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'blog.urls'
+ROOT_URLCONF = 'noticiascap.urls'
 
 TEMPLATES = [
     {
@@ -62,7 +62,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'blog.wsgi.application'
+WSGI_APPLICATION = 'noticiascap.wsgi.application'
 
 
 # Database
